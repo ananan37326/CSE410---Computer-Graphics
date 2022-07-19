c930f5dad0b24b39fb8206aa2813379b7cd75e24
ananan37326 <1705053@ugrad.cse.buet.ac.bd>
2022-07-20 04:30:39 +0600

    Struct matrix

--- Offline2/main.cpp
+++ Offline2/main.cpp
@@ -1,15 +1,8 @@
 #include<bits/stdc++.h>
 
-using namespace std;
-
-double eyeX, eyeY, eyeZ;
-double lookX, lookY, lookZ;
-double upX, upY, upZ;
-double fov;
-double aspectRatio;
-double nearPlane, farPlane;
-
 #define pi (2 * acos(0.0))
+
+using namespace std;
 
 // Struct for representing a point in 3D space
 struct point
@@ -29,6 +22,28 @@ struct point
 	}
 };
 
+struct matrix
+{
+    double mat[4][4];
+    matrix()
+    {
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 4; j++)
+                mat[i][j] = 0;
+    }
+
+    
+};
+
+double eyeX, eyeY, eyeZ;
+double lookX, lookY, lookZ;
+double upX, upY, upZ;
+double fov;
+double aspectRatio;
+double nearPlane, farPlane;
+point eye, look, up;
+
+
 // POINT ARITHMETICS
 point operator+(point a, point b)
 {
@@ -45,6 +60,37 @@ point operator*(point a, double b)
 	return point(a.x * b, a.y * b, a.z * b);
 }
 
+double dot(point a, point b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+point cross(point a, point b)
+{
+    return point(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
+}
+
+void modelingTransformation()
+{
+
+}
+
+void viewTransformation()
+{
+
+}
+
+
+
+
+void initializegluLookAtParams()
+{
+    eye = point(eyeX, eyeY, eyeZ);
+    look = point(lookX, lookY, lookZ);
+    up = point(upX, upY, upZ);
+}
+
+
 void getSceneInfo(){
     ifstream fin("scene.txt");
     if (!fin) {
@@ -58,6 +104,9 @@ void getSceneInfo(){
     fin >> fov;
     fin >> aspectRatio;
     fin >> nearPlane >> farPlane;
+    initializegluLookAtParams();
+
+
 
     fin.close();
 }
@@ -70,7 +119,11 @@ void printParams(){
     cout << "Aspect Ratio: " << aspectRatio << endl;
     cout << "Near Plane: " << nearPlane << endl;
     cout << "Far Plane: " << farPlane << endl;
+    cout << "Eye: " << eye.x << " " << eye.y << " " << eye.z << endl;
+    cout << "Look: " << look.x << " " << look.y << " " << look.z << endl;
+    cout << "Up: " << up.x << " " << up.y << " " << up.z << endl; 
 }
+
 
 int main(){
 
